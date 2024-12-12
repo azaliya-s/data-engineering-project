dea366965406d1b2c5a1d6207b2a7e1a0556daa7
azaliya-s <kdrhl06@gmail.com>
2024-12-12 16:24:39 +0500

    Update bitcoin_dbt_dag.py

--- dags/bitcoin_dbt_dag.py
+++ dags/bitcoin_dbt_dag.py
@@ -2,20 +2,18 @@ from airflow import DAG
 from airflow.providers.bash.operators.bash import BashOperator
 from datetime import datetime
 
-# Инициализация DAG
-dag = DAG(
+# Initialize DAG
+with DAG(
     'bitcoin_dbt_dag',
     description='Run DBT Models for Bitcoin Data',
     schedule_interval=None,  
     start_date=datetime(2024, 12, 12),
     catchup=False
-)
+) as dag:
+    # DBT run operator
+    dbt_run = BashOperator(
+        task_id='run_dbt_models',
+        bash_command='dbt run --profiles-dir ./dbt/bitcoin_project/profiles.yml --project-dir ./dbt/bitcoin_project',
+        dag=dag
+    )
 
-# Оператор для выполнения dbt run
-dbt_run = BashOperator(
-    task_id='run_dbt_models',
-    bash_command='dbt run --profiles-dir ./dbt/bitcoin_project/profiles.yml --project-dir ./dbt/bitcoin_project',
-    dag=dag
-)
-
-dbt_run
