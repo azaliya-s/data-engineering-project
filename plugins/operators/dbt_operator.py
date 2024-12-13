318f9ceb377b0bb1ed73235a823a7cd766ddbd74
azaliya-s <kdrhl06@gmail.com>
2024-12-13 16:22:32 +0500

    Update dbt_operator.py

--- plugins/operators/dbt_operator.py
+++ plugins/operators/dbt_operator.py
@@ -5,7 +5,14 @@ import os
 
 class DbtRunOperator(BaseOperator):
     @apply_defaults
-    def __init__(self, models=None, profiles_dir="/usr/local/airflow/dbt", project_dir="/usr/local/airflow/dbt", *args, **kwargs):
+    def __init__(
+        self,
+        models=None,
+        profiles_dir="/usr/local/airflow/dbt",
+        project_dir="/usr/local/airflow/dbt",
+        *args, 
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self.models = models
         self.profiles_dir = profiles_dir
@@ -18,12 +25,41 @@ class DbtRunOperator(BaseOperator):
         cmd += ["--profiles-dir", self.profiles_dir, "--project-dir", self.project_dir]
 
         self.log.info(f"Running DBT command: {' '.join(cmd)}")
-
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
 
         if process.returncode != 0:
             self.log.error(stderr.decode("utf-8"))
-            raise Exception(f"DBT command failed: {stderr.decode('utf-8')}")
-
+            raise Exception(f"DBT run command failed: {stderr.decode('utf-8')}")
         self.log.info(stdout.decode("utf-8"))
+
+
+class DbtTestOperator(BaseOperator):
+    @apply_defaults
+    def __init__(
+        self,
+        profiles_dir="/usr/local/airflow/dbt",
+        project_dir="/usr/local/airflow/dbt",
+        *args, 
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.profiles_dir = profiles_dir
+        self.project_dir = project_dir
+
+    def execute(self, context):
+        cmd = [
+            "dbt", "test",
+            "--profiles-dir", self.profiles_dir,
+            "--project-dir", self.project_dir
+        ]
+
+        self.log.info(f"Running DBT command: {' '.join(cmd)}")
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            self.log.error(stderr.decode("utf-8"))
+            raise Exception(f"DBT test command failed: {stderr.decode('utf-8')}")
+        self.log.info(stdout.decode("utf-8"))
+
